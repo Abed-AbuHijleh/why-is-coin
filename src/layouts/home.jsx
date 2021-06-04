287f83f850875f275d532b650d6381cbd73e0d5a
Abed-AbuHijleh <52017507+Abed-AbuHijleh@users.noreply.github.com>
2021-06-03 23:20:29 -0400

    fixed

--- src/layouts/home.jsx
+++ src/layouts/home.jsx
@@ -1,43 +1,46 @@
-import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
-import TopSearch from '../components/top-search/top-search.jsx'
-import HomePreview from '../components/home-preview/home-preview.jsx'
+import TopSearch from "../components/top-search/top-search.jsx";
+import HomePreview from "../components/home-preview/home-preview.jsx";
 
 const Home = () => {
-  const [loading, setLoading] = useState(true)
-  const [data, setData] = useState([])
-  const [total, setTotal] = useState(0)
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [total, setTotal] = useState(0);
 
-  const query = new URLSearchParams(useLocation().search)
-  const page = parseInt(query.get('page') || '1', 10)
-  const rowsPerPage = 100
+  const query = new URLSearchParams(useLocation().search);
+  const page = parseInt(query.get("page") || "1", 10);
+  const rowsPerPage = 100;
 
   const fetchHomeData = async (start, end) => {
-    let res = await new XMLHttpRequest()
-    res.onload = apiSucc
-    res.onerror = console.log()
+    let res = await new XMLHttpRequest();
+    res.onload = apiSucc;
+    res.onerror = console.log();
     res.open(
-      'GET',
-      'https://why-is-coin-api.herokuapp.com/data/latest?start=' + start + '&end=' + end,
-    )
-    res.send()
-  }
+      "GET",
+      "https://why-is-coin-api.herokuapp.com/data/latest?start=" +
+        start +
+        "&end=" +
+        end
+    );
+    res.send();
+  };
 
   function apiSucc() {
     try {
-      setTotal(JSON.parse(this.responseText)[0].total)
-      setData(JSON.parse(this.responseText))
-      setLoading(false)
+      setTotal(JSON.parse(this.responseText)[0].total);
+      setData(JSON.parse(this.responseText));
+      setLoading(false);
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
   }
 
   useEffect(() => {
-    fetchHomeData((page - 1) * rowsPerPage + 1, page * rowsPerPage)
+    fetchHomeData((page - 1) * rowsPerPage + 1, page * rowsPerPage);
     // eslint-disable-next-line
-  }, [])
+  }, []);
 
   return (
     <React.Fragment>
@@ -51,7 +54,7 @@ const Home = () => {
         fetchHomeData={fetchHomeData}
       />
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
