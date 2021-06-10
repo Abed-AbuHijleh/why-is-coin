6865b7fd922200928280bc959103877e400151ca
Abed-AbuHijleh <52017507+Abed-AbuHijleh@users.noreply.github.com>
2021-06-10 01:32:00 -0400

    damb ok

--- src/index.js
+++ src/index.js
@@ -8,18 +8,13 @@ import HttpsRedirect from "react-https-redirect";
 
 // performance
 // import reportWebVitals from './reportWebVitals';
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  // dev code
-  ReactDOM.render(<App />, document.getElementById("root"));
-} else {
-  // production code
-  ReactDOM.render(
-    <HttpsRedirect>
-      <App />
-    </HttpsRedirect>,
-    document.getElementById("root")
-  );
-}
+
+ReactDOM.render(
+  <HttpsRedirect>
+    <App />
+  </HttpsRedirect>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
